2f859dfe1899f952cd29f172056269aa80917d76
1slander <hectorgomendez@gmail.com>
2023-05-02 19:09:22 +0100

    comment test

--- models/User.model.js
+++ models/User.model.js
@@ -3,6 +3,7 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the User model to whatever makes sense in this case
 const userSchema = new Schema(
   {
+    //comment
     username: {
       type: String,
       required: false,
