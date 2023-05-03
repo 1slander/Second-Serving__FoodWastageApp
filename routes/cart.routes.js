f31447f66cec659c90932d35d32f6027030fd6e7
1slander <hectorgomendez@gmail.com>
2023-05-03 13:37:50 +0100

    fix auth

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -6,17 +6,19 @@ const CartModel = require('../models/Cart.model')
 const UserModel=require('../models/User.model')
 
 // GET
-router.get("/cart/:userId", async (req,res,next)=>{
+router.get("/", async (req,res,next)=>{
 
  // Get the user Id from the session
  const userId = req.session.currentUser._id;
+ console.log(userId);
 try{
- const userCart = await CartModel.findById(userId);
- if(!userCart){
-  res.send('You Cart is empty')
- } else {
-  res.send('here is your cart')
- }
+ res.render("cart/cart.hbs")
+ // const userCart = await CartModel.findById(userId);
+ // if(!userCart){
+ //  res.send('You Cart is empty')
+ // } else {
+ //  res.send('here is your cart')
+ // }
  
 }catch (err){
  console.log(err)
