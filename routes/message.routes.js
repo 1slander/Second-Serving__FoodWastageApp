e291d9f33a1d0f396c8e8405bb6fb2c56fb0ea05
1slander <hectorgomendez@gmail.com>
2023-05-04 17:58:18 +0100

    message route

--- routes/message.routes.js
+++ routes/message.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Message = require("../models/message");
 
+//To merge
 // Create a new message
 router.post("/messages", async (req, res) => {
   try {
