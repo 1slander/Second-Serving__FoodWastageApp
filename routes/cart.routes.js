4e3077fab6cc17844cf066aecd81e55d33e04a52
1slander <hectorgomendez@gmail.com>
2023-05-11 19:38:30 +0100

    fix id cart

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -10,15 +10,20 @@ const AdvertModel = require("../models/Advert.model");
 router.get("/", async (req, res, next) => {
   // Get the user Id from the session
   const userId = req.session.currentUser._id;
+  let price = 0;
   try {
     const userCart = await CartModel.findOne({ user: userId });
 
     if (!userCart) {
       res.render("cart/cart.hbs");
     } else {
+      userCart.products.forEach(
+        (product) => (price += product.cost * product.amount)
+      );
       res.render("cart/cart.hbs", {
         userCart,
         userInSession: req.session.currentUser,
+        price,
       });
     }
   } catch (err) {
@@ -30,20 +35,19 @@ router.get("/", async (req, res, next) => {
 
 router.post("/", async (req, res) => {
   const userId = req.session.currentUser._id;
-  let { advert: productId, title, amount, cost } = req.body;
+  let { productId: advert, title, amount, cost } = req.body;
+  console.log(req.body);
   try {
     let cart = await CartModel.findOne({ user: userId });
     console.log("This cart", cart);
 
     if (cart) {
-      cart.products.push({ productId, title, amount, cost });
+      cart.products.push({ advert, title, amount, cost });
       cart = await cart.save();
     } else {
       const newCart = await CartModel.create({
         user: userId,
-        products: [
-          { advert: productId, title: title, amount: amount, cost: cost },
-        ],
+        products: [{ advert, title, amount, cost }],
       });
     }
     res.redirect("/cart");
